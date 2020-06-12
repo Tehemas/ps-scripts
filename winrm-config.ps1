d39d2d4e71bdf216bd033ce88b5a251de65e6e0e
Thomas Lundebrekke <thomas.lundebrekke@binera.no>
2020-06-12 18:26:50 +0200

    added winrm-configuration script to config winrm using https and self-signed certificate

---
+++ winrm-config.ps1
@@ -0,0 +1,12 @@
+#This script configures winrm to only listen on HTTPS and enables a self-signed certificate to wor
+
+$hostname = $env:COMPUTERNAME
+$serverCert = New-SelfSignedCertificate -CertStoreLocation Cert:\LocalMachine\My -DnsName $hostname
+Export-Certificate -Cert $serverCert -FilePath "C:\PsRemoting-Cert.cer"
+Enable-PSRemoting -Force
+Get-ChildItem wsman:\localhost\listener | Where-Object -Property Keys -eq 'Transport=HTTP' | Remove-Item -Force
+New-Item -Path WSMan:\localhost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $serverCert.Thumbprint -Force
+New-NetFirewallRule -DisplayName "WinRM - Powershell Remote SSL-in" -Name "WinRM - Powershell remote SSLin" -Profile Any -LocalPort 5986 -Protocol TCP
+Restart-Service WinRM
+
+Write-host "Remember to add hostname: " $hostname "to hosts file if outside of domain"
